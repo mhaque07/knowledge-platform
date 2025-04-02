059b29db27a6f634bab3f242087f7913c3a77782
arpithasureshappa <arpitha.sureshappa@tarento.com>
2025-04-02 11:33:35 +0530

    added secureSettings key map in request

--- search-api/search-service/app/controllers/SearchController.scala
+++ search-api/search-service/app/controllers/SearchController.scala
@@ -61,7 +61,8 @@ class SearchController @Inject()(@Named(ActorNames.SEARCH_ACTOR) searchActor: Ac
     def searchV4() = loggingAction.async { implicit request =>
         val internalReq = getRequest(ApiId.APPLICATION_SEARCH)
         val requestMap: java.util.Map[String, Any] = internalReq.getRequest.asInstanceOf[util.Map[String, Any]]
-        requestMap.put(SearchConstants.isSecureSettingsDisabled, true)
+        val isSecureSettingsDisabled = requestMap.getOrDefault(SearchConstants.isSecureSettingsDisabled, true).asInstanceOf[Boolean]
+        requestMap.put(SearchConstants.isSecureSettingsDisabled, isSecureSettingsDisabled)
         setHeaderContext(internalReq)
         val filters = internalReq.getRequest.getOrDefault(SearchConstants.filters, new java.util.HashMap()).asInstanceOf[java.util.Map[String, Object]]
         val visibilityObject = filters.getOrDefault("visibility","")
