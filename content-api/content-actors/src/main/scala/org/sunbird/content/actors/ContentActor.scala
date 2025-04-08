1f5720d15c264eb332a8c819b2096372541852fe
arpithasureshappa <arpitha.sureshappa@tarento.com>
2025-04-08 09:28:24 +0530

    create and update API updated to save time in IST format

--- content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
@@ -29,7 +29,7 @@ import org.sunbird.graph.utils.NodeUtil
 import org.sunbird.managers.HierarchyManager
 import org.sunbird.managers.HierarchyManager.hierarchyPrefix
 
-import java.time.ZonedDateTime
+import java.time.{ZoneId, ZonedDateTime}
 import java.time.format.DateTimeFormatter
 import scala.collection.JavaConverters
 import scala.collection.JavaConverters._
@@ -72,16 +72,24 @@ class ContentActor @Inject() (implicit oec: OntologyEngineContext, ss: StorageSe
 		val endDateTimeStr = request.getRequest.getOrDefault("endDateTime", "").asInstanceOf[String]
 		if (StringUtils.isNotBlank(startDateTimeStr) && StringUtils.isNotBlank(endDateTimeStr)) {
 			try {
-				// Convert startDateTime string to epoch milliseconds
-				val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXX")
-				val startDateTime = ZonedDateTime.parse(startDateTimeStr, formatter)
-				val startDateTimeEpochMillis = startDateTime.toInstant.toEpochMilli
-				request.getRequest.put("startDateTime", startDateTimeStr)
+				val inputUtcFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXX")
+				val outputIstFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+				val istZoneId = ZoneId.of("Asia/Kolkata")
+
+				val startDateTimeUtc = ZonedDateTime.parse(startDateTimeStr, inputUtcFormatter)
+				val startDateTimeIst = startDateTimeUtc.withZoneSameInstant(istZoneId)
+				val formattedStartDateTimeIst = startDateTimeIst.format(outputIstFormatter)
+				val startDateTimeEpochMillis = startDateTimeIst.toInstant.toEpochMilli
+
+				request.getRequest.put("startDateTime", formattedStartDateTimeIst)
 				request.getRequest.put("startDateTimeInEpoch", startDateTimeEpochMillis.asInstanceOf[java.lang.Long])
 
-				val endDateTime = ZonedDateTime.parse(endDateTimeStr, formatter)
-				val endDateTimeEpochMillis = endDateTime.toInstant.toEpochMilli
-				request.getRequest.put("endDateTime", endDateTimeStr)
+				val endDateTimeUtc = ZonedDateTime.parse(startDateTimeStr, inputUtcFormatter)
+				val endDateTimeIst = endDateTimeUtc.withZoneSameInstant(istZoneId)
+				val formattedEndDateTimeIst = endDateTimeIst.format(outputIstFormatter)
+				val endDateTimeEpochMillis = startDateTimeIst.toInstant.toEpochMilli
+
+				request.getRequest.put("endDateTime", formattedEndDateTimeIst)
 				request.getRequest.put("endDateTimeInEpoch", endDateTimeEpochMillis.asInstanceOf[java.lang.Long])
 			} catch {
 				case ex: Exception =>
@@ -166,15 +174,24 @@ class ContentActor @Inject() (implicit oec: OntologyEngineContext, ss: StorageSe
 		val endDateTimeStr = request.getRequest.getOrDefault("endDateTime", "").asInstanceOf[String]
 		if (StringUtils.isNotBlank(startDateTimeStr) && StringUtils.isNotBlank(endDateTimeStr)) {
 			try {
-				// Convert startDateTime string to epoch milliseconds
-				val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXX")
-				val startDateTime = ZonedDateTime.parse(startDateTimeStr, formatter)
-				val startDateTimeEpochMillis = startDateTime.toInstant.toEpochMilli
-				request.getRequest.put("startDateTime", startDateTimeStr)
+				val inputUtcFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXX")
+				val outputIstFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+				val istZoneId = ZoneId.of("Asia/Kolkata")
+
+				val startDateTimeUtc = ZonedDateTime.parse(startDateTimeStr, inputUtcFormatter)
+				val startDateTimeIst = startDateTimeUtc.withZoneSameInstant(istZoneId)
+				val formattedStartDateTimeIst = startDateTimeIst.format(outputIstFormatter)
+				val startDateTimeEpochMillis = startDateTimeIst.toInstant.toEpochMilli
+
+				request.getRequest.put("startDateTime", formattedStartDateTimeIst)
 				request.getRequest.put("startDateTimeInEpoch", startDateTimeEpochMillis.asInstanceOf[java.lang.Long])
-				val endDateTime = ZonedDateTime.parse(endDateTimeStr, formatter)
-				val endDateTimeEpochMillis = endDateTime.toInstant.toEpochMilli
-				request.getRequest.put("endDateTime", endDateTimeStr)
+
+				val endDateTimeUtc = ZonedDateTime.parse(startDateTimeStr, inputUtcFormatter)
+				val endDateTimeIst = endDateTimeUtc.withZoneSameInstant(istZoneId)
+				val formattedEndDateTimeIst = endDateTimeIst.format(outputIstFormatter)
+				val endDateTimeEpochMillis = startDateTimeIst.toInstant.toEpochMilli
+
+				request.getRequest.put("endDateTime", formattedEndDateTimeIst)
 				request.getRequest.put("endDateTimeInEpoch", endDateTimeEpochMillis.asInstanceOf[java.lang.Long])
 			} catch {
 				case ex: Exception =>
